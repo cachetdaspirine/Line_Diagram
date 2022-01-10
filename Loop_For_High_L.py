3dd9d86da4cee5ef3eafd485c4880965a7be1a41
cachetdaspirine <leroyhugo@yahoo.fr>
2022-01-10 10:14:58 +0100

    Change the why we output the data, into non-binary obects

--- Loop_For_High_L.py
+++ Loop_For_High_L.py
@@ -6,13 +6,20 @@ nparticles = 20
 ellmin = 5
 #if not os.path.isfile('touch Seeds_With_Ell_Higher_'+str(ellmin)+'_bis.dat'):
 #    os.system('touch Seeds_With_Ell_Higher_'+str(ellmin)+'_bis.dat')
-file = open('Seeds_With_Ell_Higher_'+str(ellmin)+'_bis.dat','ab')
+file = open('Seeds_With_Ell_Higher_'+str(ellmin)+'_bis.dat','a')
 
 for i in range(nparticles):
     #SeedsFile = open('Seeds_With_Ell_Higher_'+str(ellmin)+'.dat','r')
     #Forbiden_seeds = {np.int64(f) for f in np.array(SeedsFile.read().split("\n"))[:-1]}
-    Forbiden_seeds = set(np.loadtxt('Seeds_With_Ell_Higher_'+str(ellmin)+'_bis.dat',dtype=np.int64))
+    try:
+        Forbiden_seeds = set(np.loadtxt('Seeds_With_Ell_Higher_'+str(ellmin)+'_bis.dat',dtype=np.int64))
+    except TpeError:
+        try:
+            Forbiden_seeds = set([float(np.loadtxt('Seeds_With_Ell_Higher_'+str(ellmin)+'_bis.dat',dtype=np.int64))])
+        except:
+            raise
     seed,ell0 = Get_High_L_Matrix(Forbiden_seeds = Forbiden_seeds,ellmin=ellmin)
 
-    np.savetxt(file,np.int64(np.array([seed])),fmt='%i')
+    #np.savetxt(file,np.array([seed],dtype=np.int64))
+    file.write(str(seed)+'\n')
 file.close()
