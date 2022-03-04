a02d1719fb3c77a3a8f321e4c8defc5115b11505
cachetdaspirine <leroyhugo@yahoo.fr>
2022-03-04 08:26:46 +0100

    forgot to declare Lines

--- loop_best_aggregate.py
+++ loop_best_aggregate.py
@@ -34,6 +34,7 @@ Nus = np.load(Folder+File+'NU.npy')[SimNum*Nline:(SimNum+1)*Nline]
 ells=np.load(Folder+File+'ELL.npy')[SimNum*Nline:(SimNum+1)*Nline]
 # we also stor GammaMaxs
 GammaMaxS = np.zeros(Seeds.shape[0],dtype=float)
+Lines = np.zeros(Seeds.shape[0],dtype=np.ndarray)
 
 
 for n in range(Seeds.shape[0]):
